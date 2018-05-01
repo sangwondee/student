53b4649148df59f22c069a2e76e2983d0797a4f3
sangwondee <sangwondee@gmail.com>
2018-05-01 22:52:12 +0700

    Test commit Student Project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Student Project
+                    Student Project !!!
                 </div>
 
                 <div class="links">

5f144f362794a232cfe0d67ff0ddae699661c11c
sangwondee <sangwondee@gmail.com>
2018-05-01 22:50:48 +0700

    Change Welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Student Project
                 </div>
 
                 <div class="links">
